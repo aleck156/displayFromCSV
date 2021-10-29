9214dd1624351c29e1457e3d3e5a864698f754e8
Aleksander Jaworski <aleck156@gmail.com>
2021-10-29 09:55:27 +0200

    define getTransactions function

--- app/App.php
+++ app/App.php
@@ -17,5 +17,15 @@ function getTransactionFiles(string $dirpath): array{
 }
 
 function getTransactions(string $fileName): array{
+  if (!file_exists($fileName)){
+    trigger_error("File {$fileName} does not exist.", E_USER_ERROR);
+  }
+  $file = fopen($fileName, 'r');
+  $transactions = [];
 
+  while (($transaction = fgetcsv($file)) !== false){
+    $transactions[] = $transaction;
+  }
+
+  return $transactions;
 }
