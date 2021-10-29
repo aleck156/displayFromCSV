cebb80df56ae514b222d756c266f803c73365bb1
Aleksander Jaworski <aleck156@gmail.com>
2021-10-29 09:18:31 +0200

    skip displaying folders, add file to file array

--- app/App.php
+++ app/App.php
@@ -6,6 +6,12 @@ function getTransactionFiles(): array{
   $files = [];
 
   foreach(scandir(FILES_PATH) as $file) {
+    if (is_dir($file)){
+      continue;
+    }
+
+    $files[] = $file;
+
     echo '<pre>';
     var_dump($file);
     echo '</pre>';
