0e666057e1c3fea55e72ee18196ada797e7d85fd
Aleksander Jaworski <aleck156@gmail.com>
2021-10-29 21:05:23 +0200

    refactor code - getTransactions takes transactionHandler method that reads specific file using custom-made function for that dataset

--- app/App.php
+++ app/App.php
@@ -16,7 +16,7 @@ function getTransactionFiles(string $dirpath): array{
   };
 }
 
-function getTransactions(string $fileName): array{
+function getTransactions(string $fileName, ?callable $transactionHandler = null): array{
   if (!file_exists($fileName)){
     trigger_error("File {$fileName} does not exist.", E_USER_ERROR);
   }
@@ -26,7 +26,10 @@ function getTransactions(string $fileName): array{
   fgetcsv($file);
 
   while (($transaction = fgetcsv($file)) !== false){
-    $transactions[] = extractTransaction($transaction);
+    if ($transactionHandler !== null){
+      $transaction = $transactionHandler($transaction);
+    }
+    $transactions[] = $transaction;
   }
 
   return $transactions;
